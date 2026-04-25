4283f727a6f9e52382f439d0961672283cee3af6
integricai <nadir.khan@integricai.co.uk>
2026-04-25 18:36:16 +0100

    fixed issue for about us home page

---
+++ api/publish-feature-cards-2.js
@@ -0,0 +1,7 @@
+import { createCmsValueHandler } from './_cms-blob-store.js';
+
+export default createCmsValueHandler({
+  blobFile: 'vls-feature-cards-2.json',
+  key: 'sections',
+  emptyValue: []
+});
