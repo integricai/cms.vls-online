b5d35c10d5ca162e24f83f92a7747a14ac7cec91
integricai <nadir.khan@integricai.co.uk>
2026-05-01 12:14:56 +0100

    Added a new hero banner for static pages

---
+++ api/publish-page-hero-banner-components.js
@@ -0,0 +1,2 @@
+import { createCmsValueHandler } from './_cms-blob-store.js';
+export default createCmsValueHandler({ blobFile: 'vls-page-hero-banner-components.json', key: 'components', emptyValue: [] });
