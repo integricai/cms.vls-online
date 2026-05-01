585054b83afbec811ecc3d1323c4881a0e3c50c8
integricai <nadir.khan@integricai.co.uk>
2026-05-01 12:28:31 +0100

    Added a new banner section left pane for the static pages

---
+++ api/publish-page-left-hero-components.js
@@ -0,0 +1,2 @@
+import { createCmsValueHandler } from './_cms-blob-store.js';
+export default createCmsValueHandler({ blobFile: 'vls-page-left-hero-components.json', key: 'components', emptyValue: [] });
