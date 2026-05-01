d47fb157d18e7cf09ee769323290ce90afb6e1c9
integricai <nadir.khan@integricai.co.uk>
2026-05-01 15:39:54 +0100

    program cards with images

---
+++ api/publish-program-cards-v2.js
@@ -0,0 +1,7 @@
+import { createCmsValueHandler } from './_cms-blob-store.js';
+
+export default createCmsValueHandler({
+  blobFile: 'vls-program-cards-v2.json',
+  key: 'sections',
+  emptyValue: []
+});
