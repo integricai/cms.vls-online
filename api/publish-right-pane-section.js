d31685664ba49cd31275c804eb1403451a2ebc73
Muhammad Nadir Khan <nadir.khan@integricai.co.uk>
2026-05-02 12:17:56 +0100

    Added another pane for generic page

---
+++ api/publish-right-pane-section.js
@@ -0,0 +1,7 @@
+import { createCmsValueHandler } from './_cms-blob-store.js';
+
+export default createCmsValueHandler({
+  blobFile: 'vls-right-pane-section.json',
+  key: 'sections',
+  emptyValue: []
+});
